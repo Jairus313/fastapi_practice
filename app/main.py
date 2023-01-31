57b94f2613229f808dbd7381efdf72d0d898946e
Jairus313 <sudeep.nellue.7@gmail.com>
2023-01-31 16:19:35 +0530

    delete & update

--- app/main.py
+++ app/main.py
@@ -57,3 +57,34 @@ async def get_post(id: int, db: Session = Depends(get_db)):
         )
 
     return {"post_detail": post}
+
+@app.delete("/posts/{id}")
+async def delete_post(id: int, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == id)
+    
+    if not post.first():
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail = "Post does not exist"
+        )
+
+    post.delete(synchronize_session = False)
+    db.commit()
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put("/posts/{id}")
+async def update_post(id: int, post: Post, db: Session = Depends(get_db)):
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+
+    if not post_query.first():
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail = "Post does not exist"
+        )
+
+    post_query.update(post.dict(), synchronize_session= False)
+
+    db.commit()
+
+    return {"data": post_query.first()}
