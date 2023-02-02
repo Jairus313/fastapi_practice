6b154eb85bf11149dd352ae77966cbb5ab844e8a
Jairus313 <sudeep.nellue.7@gmail.com>
2023-02-02 19:46:17 +0530

    oauth2

--- app/routers/post.py
+++ app/routers/post.py
@@ -27,7 +27,7 @@ def get_posts(db: Session = Depends(get_db)):
 
 @router.post("/", status_code = status.HTTP_201_CREATED, response_model = schemas.PostResponse)
 async def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), 
-    user_id: int = Depends(oauth2.get_current_user)):
+    current_user: int = Depends(oauth2.get_current_user)):
     new_post = models.Post(
                         **post.dict()
                         )
@@ -40,7 +40,7 @@ async def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db),
     return new_post
 
 @router.get("/{id}", response_model = schemas.PostResponse)
-async def get_post(id: int, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
+async def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     post = db.query(models.Post).filter(models.Post.id == id).first()
 
     if not post:
@@ -52,7 +52,7 @@ async def get_post(id: int, db: Session = Depends(get_db), user_id: int = Depend
     return post
 
 @router.delete("/{id}", response_model = schemas.PostResponse)
-async def delete_post(id: int, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
+async def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     post = db.query(models.Post).filter(models.Post.id == id)
     
     if not post.first():
@@ -67,7 +67,7 @@ async def delete_post(id: int, db: Session = Depends(get_db), user_id: int = Dep
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @router.put("/{id}", response_model = schemas.PostResponse)
-async def update_post(id: int, post: schemas.PostBase, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
+async def update_post(id: int, post: schemas.PostBase, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     post_query = db.query(models.Post).filter(models.Post.id == id)
 
     if not post_query.first():
